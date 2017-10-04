68d9cde8401c547a982ff58e0d84deb689bc1e4d
inigo10rodri <inigo10rodri@gmail.com>
2017-10-04 04:57:19 -0400

    Validation

--- resources/views/mensajeAgur.php
+++ resources/views/mensajeAgur.php
@@ -1,8 +1,12 @@
 <h1>Agur</h1>
 <?php
+
+
 //Isset te devuelve si existe el elemento indicado en este caso si se ha enviado el POST con el nombre correo
-if (isset($_POST["correo"])) {
-	echo $_POST['correo'];
+if (isset($_POST["correo"]) && isset($_POST["nombre"])) {
+	echo $_POST['correo'].'<br>';
+    echo $_POST['nombre'];
 }
+
 ?>
 
--- resources/views/postForm.blade.php
+++ resources/views/postForm.blade.php
@@ -1,10 +1,18 @@
 
-
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 <form method=post action="/mensajeAgur">
 	{{ csrf_field()}}
-    	<input type= "email" name="correo" value="Correo electronico" required></input>
-		<input type="text" name="nombre" required></input>
+    	<input type= "email" name="correo" value="Correo electronico" required></input><br>
+		<input type="text" name="nombre" value="Nombre" required></input><br>
 		<input type="submit" name="aceptar" value="Enviar"></input>
-	
+
 </form>
 
